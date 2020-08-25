cf512aee5acf88a11970f0c4caac0a35d98adb16
agbleze <62272559+agbleze@users.noreply.github.com>
2020-08-25 23:41:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ navToggle.addEventListener('click', () => {
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-
+        document.body.classList.remove('nav-open');
     })
 })
 
