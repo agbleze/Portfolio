f5a2dace62ab2fbee5c5608013d8f02b209157f5
agbleze <62272559+agbleze@users.noreply.github.com>
2020-08-25 23:32:36 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector('nav-toggle');
 document.querySelector('.nav-link')
 
 navToggle.addEventListener('click', () => {
-
+    document.body.classList.toggle('nav-open');
 })
 
 
